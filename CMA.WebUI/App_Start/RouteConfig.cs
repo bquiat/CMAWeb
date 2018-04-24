88b7fcef233bfdd30ec97d3e5b24e46b50ac33df
Manish <manigarg@gmail.com>
2018-04-24 09:03:35 -0700

    rr

--- CMA.WebUI/App_Start/RouteConfig.cs
+++ CMA.WebUI/App_Start/RouteConfig.cs
@@ -11,6 +11,7 @@ namespace CMA.WebUI
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.MapRoute(
                 name: "DefaultRoute",
